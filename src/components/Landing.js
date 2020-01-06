a8ade94201ce9a576f13b03a99b602d9710be2e4
Christopher Willis <willis.christopher.a@gmail.com>
2020-01-06 17:08:55 -0600

    landing page mostly framed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,28 @@
 
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import {Link} from "react-router-dom";
 
 function Landing() {
   return (
-    <div>
-      <h1>Arkham Horror</h1>
-      <p>
-        Campaign Guide
-      </p>
-    </div>
+      <Container fluid  className="text-center  h-100">
+        <Row className="flex-column   h-100">
+          <Col >
+            <Link className="override p-2" to="/NewCampaign">New Game</Link>
+          </Col>
+          <Col className="">
+            <Link className="override p-2" to="/">Load Game</Link>
+          </Col>
+          <Col className="text-center">
+            <Link className="override p-2" to="/">Standalone Scenario</Link>
+          </Col>
+          <Col className="text-center">
+            <Link className="override p-2" to="/">Chaos Bag</Link>
+          </Col>
+        </Row>
+      </Container>
   )
 }
 
