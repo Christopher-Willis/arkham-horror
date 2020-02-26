ae78925c92fd325676b513184c1db3d4fe2bfd14
Christopher Willis <willis.christopher.a@gmail.com>
2020-02-25 21:34:49 -0600

    Updates

---
+++ precache-manifest.7f051c8a0afdfaa14ba4bcafc0e57bf5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f7d2639b04cd3e4e8499bbcdd09186",
+    "url": "/arkham-horror/index.html"
+  },
+  {
+    "revision": "c8be87e7bc9b1dc289e0",
+    "url": "/arkham-horror/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5964615174afa77702df",
+    "url": "/arkham-horror/static/css/main.4a7dcf66.chunk.css"
+  },
+  {
+    "revision": "c8be87e7bc9b1dc289e0",
+    "url": "/arkham-horror/static/js/2.2a74301b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/arkham-horror/static/js/2.2a74301b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5964615174afa77702df",
+    "url": "/arkham-horror/static/js/main.74b9991e.chunk.js"
+  },
+  {
+    "revision": "11b87c9f9d4b6cb84b45",
+    "url": "/arkham-horror/static/js/runtime-main.4f7c4520.js"
+  },
+  {
+    "revision": "65124630a6989df2a67a08f01c582c10",
+    "url": "/arkham-horror/static/media/bruno-magalhaes-vianna-yog-sothoth.65124630.jpg"
+  },
+  {
+    "revision": "6360bb2d58e78979cb67eeab30bcdefd",
+    "url": "/arkham-horror/static/media/dialog_screen_bg.6360bb2d.png"
+  },
+  {
+    "revision": "ff413f851c1bd21a51ca651a6b6542b1",
+    "url": "/arkham-horror/static/media/guardian.ff413f85.png"
+  },
+  {
+    "revision": "06a54c797bfb995109cc8b53b78f9eb5",
+    "url": "/arkham-horror/static/media/mystic.06a54c79.png"
+  },
+  {
+    "revision": "ca8ce3b910809dc59e0bacca211e1104",
+    "url": "/arkham-horror/static/media/neutral.ca8ce3b9.svg"
+  },
+  {
+    "revision": "ea5b101b89e946315edd3806c0121f9b",
+    "url": "/arkham-horror/static/media/normal_screen_bg.ea5b101b.png"
+  },
+  {
+    "revision": "9403ccb727125cc2ca712ff1d6e007d3",
+    "url": "/arkham-horror/static/media/rogue.9403ccb7.png"
+  },
+  {
+    "revision": "784fe13f5d27304b592d257489aa9526",
+    "url": "/arkham-horror/static/media/seal_tokenautofail.784fe13f.png"
+  },
+  {
+    "revision": "9cfee7d4487d59c71ea1fc12ad6630fb",
+    "url": "/arkham-horror/static/media/seal_tokencultist.9cfee7d4.png"
+  },
+  {
+    "revision": "cde9c7be377f073fd476aa82cb38d5fb",
+    "url": "/arkham-horror/static/media/seal_tokeneldar.cde9c7be.png"
+  },
+  {
+    "revision": "c1196eb6d3c9f8eced60235da0529206",
+    "url": "/arkham-horror/static/media/seal_tokenskull.c1196eb6.png"
+  },
+  {
+    "revision": "6cfc1ac1c8f5296c4e664211517ddb21",
+    "url": "/arkham-horror/static/media/seal_tokentablets.6cfc1ac1.png"
+  },
+  {
+    "revision": "41f8219aa69d4e419397d3689c918198",
+    "url": "/arkham-horror/static/media/seal_tokenthingy.41f8219a.png"
+  },
+  {
+    "revision": "e02964bc1fca835ce99c6761c9559fb7",
+    "url": "/arkham-horror/static/media/seeker.e02964bc.png"
+  },
+  {
+    "revision": "646cdf02c8ba2f48f77c5287c39f66de",
+    "url": "/arkham-horror/static/media/survivor.646cdf02.png"
+  },
+  {
+    "revision": "7c7899aeb29dcf6bf6d98c2b012b48d7",
+    "url": "/arkham-horror/static/media/tokenautofail.7c7899ae.png"
+  },
+  {
+    "revision": "fc0928b9ed91d9b3d1834dc847f4e624",
+    "url": "/arkham-horror/static/media/tokencultist.fc0928b9.png"
+  },
+  {
+    "revision": "b568a2efbfb2a857d8fc45a3220c3016",
+    "url": "/arkham-horror/static/media/tokeneldar.b568a2ef.png"
+  },
+  {
+    "revision": "b682d6fc9f1ca71f8107dd050e9d6588",
+    "url": "/arkham-horror/static/media/tokenskull.b682d6fc.png"
+  },
+  {
+    "revision": "985442e7e71d503f00f8ef770e7000b2",
+    "url": "/arkham-horror/static/media/tokentablets.985442e7.png"
+  },
+  {
+    "revision": "4b4612a36aa082672042e2b8a5ecd12f",
+    "url": "/arkham-horror/static/media/tokenthingy.4b4612a3.png"
+  }
+]);
